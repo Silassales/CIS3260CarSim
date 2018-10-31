4608a2dcba7396cf49b78b970646e0dc5184bb40
Matt Falkner <baron@Matts-MacBook-Pro.local>
2018-10-31 12:44:05 -0400

    Built framework of model from UML digram to Matt Branch

--- src/model/CarState.java
+++ src/model/CarState.java
@@ -1,5 +1,78 @@
 package model;
 
 public class CarState {
+	
+	/* The overrides are to ensure no over/under flow gears  */
+	public enum transmissionGear { 
+		Park { 
+			@Override 
+			public transmissionGear prev() {
+				//You cannot shift below Park, stay the same
+				return this; 
+			};	
+		}, 
+		Neutral, 
+		First, 
+		Second, 
+		Third, 
+		Fourth, 
+		Fifth { 
+			@Override 
+			public transmissionGear next(){
+				//You cannot shift up passed 5th, stay the same
+				return this; 
+			};
+			
+		};
+		public transmissionGear next() { 
+			//next will return the next gear, IE. From Park -> Neutral -> First.. Etc. 
+			return values()[ordinal() + 1]; 
+		}
+		public transmissionGear prev()  {
+			//return the previous gear, IE. From Neutral -> Park, or Third to 2nd.. etc. 
+			return values() [ordinal() - 1];
+		}
+	}
+	
+	public transmissionGear transGear; 
+	
+	/* Objects */ 
+	public Location location = null;//Using builder in initialize to populate
+	public Engine engine = null; 	//Using builder in initialize to populate
+
+	
+	public Double gasLevel = null; 
+	public Double structuralIntegrity = null;
+	//Can be neg and pos to represent left and right deviation? 
+	public Double frontWheelDeviation = null; 
+	
+    public void shiftGearUp() {	
+    	//Make add a 3% chance the car just randomly goes into neutral and stalls?? :) 
+		this.transGear.next(); 
+	}
+	public void shiftGearDown() {
+		this.transGear.prev(); 
+	}
+	
+	/* Built car has is OFF, in PARK, room temperature, rpm is 0, and in default location */
+	public CarState() {
+		engine = new Engine.EngineBuilder()	//Engine built as off, or false
+				.withisEngineOn(false) 
+				.withRpm(0.0)
+				.withTemperatureC(20.0)
+				.build(); 
+		location = new Location.LocationBuilder()
+				.withDirectionDegrees(0.0)
+				.withLatitude(0.0)
+				.withLongitude(0.0)
+				.withSpeedms(0.0)
+				.build();
+		transGear = transmissionGear.Park; 
+		gasLevel = 100.0; 
+		structuralIntegrity = 100.0; 
+		frontWheelDeviation = 0.0; 
+		
+		
+	}
 
 }
--- src/model/Engine.java
+++ src/model/Engine.java
@@ -1,5 +1,55 @@
 package model;
 
 public class Engine {
-
+	
+	private Double rpm = null; 
+	private Double temperatureC = null;  
+	private Boolean isEngineOn = null; 
+	
+	public void startEngine() {
+		this.isEngineOn = true; 
+	}
+	public void stopEngine() {
+		this.isEngineOn = false; 
+	}
+	public double getRpm() {
+		return this.rpm; 
+	}
+	public boolean isEngineOn() {
+		return this.isEngineOn;
+	}
+	
+	private Engine(EngineBuilder builder) {
+		this.rpm = builder.rpm;
+		this.temperatureC = builder.temperatureC; 
+		this.isEngineOn = builder.isEngineOn;
+	}
+	
+	public static class EngineBuilder {
+		private Double rpm; 
+		private Double temperatureC;
+		private Boolean isEngineOn; 
+		
+		public EngineBuilder withisEngineOn(boolean isEngineOn) {
+			this.isEngineOn = isEngineOn; 
+			return this; 
+		}
+		
+		public EngineBuilder withRpm(double rpm) {
+			this.rpm = rpm; 
+			return this; 
+		}
+		public EngineBuilder withTemperatureC(double temperatureC)
+		{
+			this.temperatureC = temperatureC; 
+			return this; 
+		}
+		
+		public Engine build() {
+			return new Engine(this); 
+		}
+	
+		
+	}
+	
 }
