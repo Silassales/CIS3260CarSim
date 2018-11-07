30da15b7fd98b36187ade6148d32fe638c8e13eb
Matt Falkner <baron@Matts-MacBook-Pro.local>
2018-11-07 15:54:19 -0500

    Adding rough simulation model, needs to be updated to follow abstract factory design pattern

--- src/model/SimulationModel.java
+++ src/model/SimulationModel.java
@@ -2,4 +2,24 @@ package model;
 
 public class SimulationModel {
 
+	public CarState carState = null; 
+	private long time_ms = 0; 
+	
+	public void iterateSimulation(long delta_ms) {
+		this.time_ms += delta_ms; 
+		//call methods to iterate all car state variables etc. 
+	}
+	
+	public SimulationModel() throws NullDataFoundAtBuild {
+	
+		carState = new CarState.CarStateBuilder()
+				.withEngine(0, 0, false)
+				.withLocation(0, 0, 0, 0, 0)
+				.withTransmission(5, 0, false)
+				.withGasLevel(100)
+				.withFrontWheelDeviation(0)
+				.withStructuralIntegrity(100)
+				.build(); 
+	}
+	
 }
