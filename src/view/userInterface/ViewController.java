5021ba2c832df1e3554371eeb838923218af8d31
Silas Sales <silas.timothy10@gmail.com>
2018-11-07 11:28:07 -0500

    view controller POC

--- src/view/userInterface/ViewController.java
+++ src/view/userInterface/ViewController.java
@@ -4,11 +4,21 @@ import controller.ControlCenter;
 import controller.InputEvent;
 import controller.InputEventType;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class Controller {
+public class ViewController {
+
+    @FXML
+    private Button testButton;
 
     public void testAction(ActionEvent actionEvent) {
         ControlCenter.getControlCenter().handleUserInput(new InputEvent(InputEventType.TEST));
     }
+
+    public void testUpdate() {
+        testButton.setText("UPDATEDDDDD!");
+    }
+
 }
